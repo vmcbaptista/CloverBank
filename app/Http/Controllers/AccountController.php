e1b538ec61ff5a1c56bfacc964d79dcd120cf777
Vítor Baptista <vmcbaptista95@gmail.com>
2016-10-25 21:50:29 +0100

    Create new account form view

---
+++ app/Http/Controllers/AccountController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class AccountController extends Controller
+{
+    public function showForm(Request $request) {
+        return view('account.formAdd');
+    }
+}
---
+++ resources/views/account/formAdd.blade.php
@@ -0,0 +1,12 @@
+@extends('template')
+@section('content')
+<form>
+    <label>Tipo de Conta</label><br>
+    <select></select><br>
+    <label>Cliente</label><br>
+    <select></select><br>
+    <label>Depósito Inicial</label><br>
+    <input><br>
+    <button type="submit">Criar nova conta</button>
+</form>
+@endsection
