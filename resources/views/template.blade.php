67aeeffd17acac8c81392aa98a4149b494e6acb0
Vítor Baptista <2054313@student.uma.pt>
2016-11-06 12:14:20 +0000

    change source of jquery

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script type="text/javascript" src="{{ URL::asset('js/jquery-3.1.1.js') }}"></script>
+    <script type="text/javascript" src="/js/jquery-3.1.1.js"></script>
     <title>CloverBank</title>
 </head>
 <body>
