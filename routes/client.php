6065febf88e5119b3adeca2b325feb2d96dbe173
Vítor Baptista <vmcbaptista95@gmail.com>
2016-11-12 15:26:15 +0000

    Reverted changes on  routes

--- routes/client.php
+++ routes/client.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::get('/', function () {
+Route::get('/home', function () {
     $users[] = Auth::user();
     $users[] = Auth::guard()->user();
     $users[] = Auth::guard('client')->user();
