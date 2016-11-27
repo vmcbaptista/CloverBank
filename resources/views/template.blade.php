08af0f1639d455412d2f90571cf4d6299ef30488
Vítor Baptista <vmcbaptista95@gmail.com>
2016-11-27 18:44:16 +0000

    Images for products

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -86,6 +86,49 @@
                     <i class="fa fa-map-marker fa-2x" aria-hidden="true"></i>
                 </div>
 
+                <div class="contact_msg">
+                    <p>Nos nossos Balcoes</p>
+                    <p>Encontre-nos num balcao perto de si</p>
+                </div>
+            </li>
+            <li>
+                <div class="icon-contact">
+                    <i class="fa fa-comments fa-2x" aria-hidden="true"></i>
+                </div>
+                <div class="contact_msg">
+                    <p>O nosso chat</p>
+                    <p> Todos os dias 24/7 </p>
+                </div>
+            </li>
+            <li>
+                <div class="icon-contact">
+                    <i class="fa fa-envelope-o fa-2x" aria-hidden="true"></i>
+                </div>
+                <div class="contact_msg">
+                    <p>Por email</p>
+                    <p>Pelo nosso email</p>
+                </div>
+            </li>
+            <li>
+                <div class="icon-contact">
+                    <i class="fa fa-mobile fa-2x" aria-hidden="true"></i>
+                </div>
+                <div class="contact_msg">
+                    <p>700 000 000</p>
+                    <p>Atendimento a medida 24/7</p>
+                </div>
+            </li>
+
+        </ul>
+
+        <div class="social_networks">
+            <a href="https://www.facebook.com"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+            <a href="https://www.twitter.com"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
+            <a href="https://www.youtube.com"><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
+            <a href="https://plus.google.com"><i class="fa fa-google-plus-official" aria-hidden="true"></i></a>
+            <a href="https://www.linkedin.com/"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+        </div>
+    </div>
 
         <script type="text/javascript" src="{{ URL::asset('js/login_form.js') }}"></script>
         @yield('javascript')
